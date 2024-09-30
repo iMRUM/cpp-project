a851bdc6abe5aef3cdfa908fdffff47cfed94c79
imry <rodkin.imry@gmail.com>
2024-09-30 18:25:44 +0300

    directories are rearranged

--- UI/GeneralUI.hpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by imry on 9/29/24.
-//
-//Common UI for all players, will contain which squares are taken alreay,  how many players remaining etc.
-#ifndef GENERALUI_HPP
-#define GENERALUI_HPP
-
-#endif //GENERALUI_HPP
