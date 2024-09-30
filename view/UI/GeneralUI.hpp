fcd61ae709c189d2ce77d427f8711d8be7f6fc08
imry <rodkin.imry@gmail.com>
2024-09-30 18:24:04 +0300

    directories are rearranged

---
+++ view/UI/GeneralUI.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Imry Rodkin.
+//
+//    Common UI for all players,will contain which squares are taken alreay, how many players remaining etc.
+#ifndef GENERALUI_HPP
+#define GENERALUI_HPP
+
+#endif //GENERALUI_HPP
---
+++ view/UI/PlayerUI.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Imry Rodkin on 9/29/24.
+//
+// Player specified UI, such that each Player can see their own balance, Streets etc.
+#ifndef PLAYERUI_HPP
+#define PLAYERUI_HPP
+
+#endif //PLAYERUI_HPP
