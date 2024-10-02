179ab690042cc7991c92be5aaa862b8f3d458146
imry <rodkin.imry@gmail.com>
2024-10-02 11:21:50 +0300

    directories are rearranged

---
+++ model/Components/SetTradeable.hpp
@@ -0,0 +1,24 @@
+//
+// Created by imry on 10/2/24.
+//
+
+#ifndef SETTRADEABLE_HPP
+#define SETTRADEABLE_HPP
+#include "Set.h"
+
+using namespace std;
+
+class SetTradeable : public Set{
+    vector<shared_ptr<Tradeable>> _tradeables;
+    public:
+    SetTradeable(int color, const string &name, const vector<shared_ptr<Tradeable>> &tradeables): Set(color, name) {
+        for (const auto& tradeable : tradeables) {
+            _tradeables.push_back(tradeable);
+        }
+    }
+
+};
+
+
+
+#endif //SETTRADEABLE_HPP
